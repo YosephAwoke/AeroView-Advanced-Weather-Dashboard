9e78d254b07bdace157d2b729a0920c7d72a33e8
YosephAwoke <yosephawoke8@gmail.com>
2026-07-16 19:30:05 +0800

    add fontend link to allow list

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ app.set('trust proxy', 1);
 
 const allowedOrigins = new Set(
   [
+    'https://aeroview-advanced-weather-dashboard.vercel.app',
     'http://localhost:5173',
     'http://localhost:4173',
     'http://127.0.0.1:5173',
